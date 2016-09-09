b390e8e7113af7ad189db2b50f4a51bef83c9bc5
Reynaldo Juan Pereira Heredia <darkdeymon04@gmail.com>
2016-09-09 15:47:36 -0400

    cambios 4

--- app/src/main/java/com/example/darkdeymon/recuperacionjson/asincrono.java
+++ app/src/main/java/com/example/darkdeymon/recuperacionjson/asincrono.java
@@ -1,14 +1,19 @@
 package com.example.darkdeymon.recuperacionjson;
 
 import android.os.AsyncTask;
+import android.util.Base64;
+import android.util.Base64OutputStream;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -34,6 +39,28 @@ public class asincrono extends AsyncTask <URL,Void, JSONArray>{
         URL url=params[0];
         try {
             HttpURLConnection res=(HttpURLConnection)url.openConnection();
+
+            String userCredentials = "darkdeymon:nadaesrealasies";
+
+            /* autenticacion*/
+            String basicAuth = "Basic " + Base64.encodeToString(userCredentials.getBytes(),Base64.NO_WRAP | Base64.URL_SAFE);
+            Log.e("pass", basicAuth);
+            res.setRequestProperty ("Authorization", basicAuth);
+
+            /*
+            res.setRequestMethod("GET");
+            OutputStream os = res.getOutputStream();
+            OutputStreamWriter o = new OutputStreamWriter(os, "UTF-8");
+
+            JSONObject a= new JSONObject();
+            a.put("password","nadaesrealasies");
+            a.put("username","darkdeymon");
+            o.write(a.toString());
+            o.close();
+            os.close();
+            res.getResponseCode();
+            */
+
             BufferedReader read=new BufferedReader(new InputStreamReader(res.getInputStream()));
             String jsonText = readAll(read);
             Log.e("jason", jsonText);
